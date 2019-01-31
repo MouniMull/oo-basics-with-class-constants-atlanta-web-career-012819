bff8eab8f878b0f76f96ca704dc99dcd2b20716d
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-31 12:32:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,8 @@ class Book
   attr_accessor :author, :page_count
   attr_reader :title, :genre 
 
+GENRES = []
+
   def initialize(title)
     @title = title
   end
@@ -10,8 +12,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
-  def genre=(genre)
-  @genre = genre 
-  GENRES << genre 
-  end 
+ def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
 end
